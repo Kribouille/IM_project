4be9260bc14cf964d237cc4c9b2e0b51f44f70ea
Kribouille <benjamin.vion1@gmail.com>
2017-01-17 15:42:56 +0100

    DateFormatter amélioré, merci Steven ;)

--- TwitterAcceleo/src/org/eclipse/acceleo/module/sample/DateFormatter.java
+++ TwitterAcceleo/src/org/eclipse/acceleo/module/sample/DateFormatter.java
@@ -1,30 +1,13 @@
 package org.eclipse.acceleo.module.sample;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
-import org.eclipse.uml2.uml.Class;
-import org.eclipse.uml2.uml.Stereotype;
 
 public class DateFormatter {
-	public String formatDate(String source) {
-		final String OLD_FORMAT = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+	public String formatDate(Date source) {
 		final String NEW_FORMAT = "yyyy-MM-dd";
-
-		String newDateString = "";
-
-		SimpleDateFormat sdf = new SimpleDateFormat(OLD_FORMAT);
-		Date d;
-		try {
-			d = sdf.parse(source);
-			sdf.applyPattern(NEW_FORMAT);
-			newDateString = sdf.format(d);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		return newDateString;
+		SimpleDateFormat f = new SimpleDateFormat(NEW_FORMAT);
+		return f.format(source);
 	}
 }
